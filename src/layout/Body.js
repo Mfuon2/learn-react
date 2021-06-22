8e9344f38cad47844270d9439bd5257a7f76a0d5
Mfuon Leonard <leonard.oduor@apollo.co.ke>
2021-06-22 15:30:04 +0300

    custom hooks

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -1,35 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useFetch from "../customHooks/useFetch";
 import BlogList from "../templates/BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(true);
   const [title, setTitle] = useState("All Blogs");
-
-  useEffect(() => {
-    // any time affter rendering
-    // any code after rerender
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogsww")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could Not fetch data from the resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setError(null);
-          setIsPending(false);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []); //will run only when title changes [] will run only once. the firs render
-
+  const {data: blogs, error, isPending} = useFetch('http://localhost:8000/blogs')
   return (
     <div className="home">
       {error && <p>{error}</p>}
