980606eae59367baddc50ca5bacf60bbaee6803b
Mfuon Leonard <leonard.oduor@apollo.co.ke>
2021-06-22 11:16:18 +0300

    use effect dependancies

---
+++ src/layout/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    const title = 'R-App'
+    return ( 
+        <nav className='navbar'>
+            <h1>
+                {title}
+            </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
